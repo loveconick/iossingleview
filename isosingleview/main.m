37e006362b18772874a78d88a91613c598c943e0
loveconick <21597375@qq.com>
2018-04-03 14:06:56 +0800

    12

--- isosingleview/main.m
+++ isosingleview/main.m
@@ -1,10 +1,3 @@
-//
-//  main.m
-//  isosingleview
-//
-//  Created by zhangzhiyong on 2018/3/23.
-//  Copyright © 2018年 zhangzhiyong. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
@@ -15,19 +8,26 @@ void callfunc(void)
 	fopen("abc.txt", "r");
 }
 
+int add(int a, int b, int c, int d, int e, int f, int g)
+{
+	return(a+b+c+d+e+f+g);
+}
+
 #pragma mark 非常重要
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        int a, b, c;
-        a = 3;
-        b = 4;
+		int a, b, c;
+		c = add(1,2,3,4,5,6,7);
+		printf("%d\n",c);
+		a = 3;
+		b = 4;
 		if (a-b) {
 			printf("a < b\n");
 		}
-        c = a + b;
-        NSLog(@"%d\n", c);
-        NSLog(@"hello everyone");
+		c = a + b;
+		NSLog(@"%d\n", c);
+		NSLog(@"hello everyone");
 		callfunc();
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+		return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
