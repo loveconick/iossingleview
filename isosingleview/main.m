3480a67391d98e3d2f2e5085c666b44eb305c65d
loveconick <21597375@qq.com>
2018-04-04 23:49:37 +0800

    清明节前
    不论是模拟器还是真机，调试都会导致XCode失去响应，无法终止调试

--- isosingleview/main.m
+++ isosingleview/main.m
@@ -1,4 +1,3 @@
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
@@ -13,10 +12,24 @@ int add(int a, int b, int c, int d, int e, int f, int g)
 	return(a+b+c+d+e+f+g);
 }
 
+@interface NumUtil : NSObject
+- (int) addValue:(int) a andValue:(int) b;
+@end
+
+@implementation NumUtil
+- (int) addValue:(int) a andValue:(int) b
+{
+	int c = a + b;
+	return(c);
+}
+@end
+
 #pragma mark 非常重要
 int main(int argc, char * argv[]) {
     @autoreleasepool {
 		int a, b, c;
+		NumUtil *pNum = [NumUtil new];
+		c = [pNum addValue:3 andValue:4];
 		c = add(1,2,3,4,5,6,7);
 		printf("%d\n",c);
 		a = 3;
